f72dfeb2d1fcfd466f3465e5ed530eec132522c4
Mainak Jas <mainakjas@gmail.com>
2016-04-18 16:21:12 +0200

    add setup.sh

---
+++ kaggle_grasp_lift/setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+spacer="========================================"
+
+mkdir -p data/train
+mkdir -p data/test
+mkdir -p data/preprocessed
+mkdir -p data/torch
+mkdir -p cv
+mkdir -p tmp
+mkdir -p tmp/sampled_files
+mkdir -p tmp/validation_files
+mkdir -p tmp/submission_files
+
+while [ ! "$(ls -A data/train)" ]; do
+    echo "Please copy the data into:"
+    echo "* data/train"
+    echo "* data/test"
+    echo "and press RETURN"
+    read _
+done
+
+if [ ! -e "data/mean_std.pickle" ]; then
+    echo "Calculating mean and std"
+    python python_utils/calc_mean_std.py
+    echo "Done"
+fi
+
+echo "Filtering and processing the data"
+echo $spacer
+if [ "$1" ]; then
+  python python_utils/modify_data.py -num_val 2 -subsample 3 -subject $1
+else
+  python python_utils/modify_data.py -num_val 2 -subsample 3
+fi
+
+echo "Setup done!"
